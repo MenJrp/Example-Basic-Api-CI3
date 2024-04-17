dd91c705973fc24f412c5224c14b6b192d7e45a9
Jirapan.ro <166211543+MenJrp@users.noreply.github.com>
2024-04-17 09:11:26 +0700

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -95,25 +95,3 @@ $db['default'] = array(
 	'failover' => array(),
 	'save_queries' => TRUE
 );
-
-// $db['default'] = array(
-// 	'dsn'	=> '',
-// 	'hostname' => 'db-personal.rmuti.ac.th',
-// 	'username' => 'jirapan.ro',
-// 	'password' => 'm48826',
-// 	'database' => 'jirapan_ro',
-// 	'dbdriver' => 'mysqli',
-// 	'dbprefix' => '',
-// 	'pconnect' => FALSE,
-// 	'db_debug' => (ENVIRONMENT !== 'production'),
-// 	'cache_on' => FALSE,
-// 	'cachedir' => '',
-// 	'char_set' => 'utf8',
-// 	'dbcollat' => 'utf8_general_ci',
-// 	'swap_pre' => '',
-// 	'encrypt' => FALSE,
-// 	'compress' => FALSE,
-// 	'stricton' => FALSE,
-// 	'failover' => array(),
-// 	'save_queries' => TRUE
-// );
